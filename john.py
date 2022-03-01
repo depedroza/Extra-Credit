b3487feefa18741f5ec674026553cacf92e0d31d
Daniel Pedroza <Daniel_Pedroza1@baylor.edu>
2022-03-01 13:20:44 -0600

    finished dictionary XC

---
+++ john.py
@@ -0,0 +1,12 @@
+john_infile = open("John.txt", "r")
+
+search = ["Father", "God", "Christ", "Spirit", "spirit", "life", "man"]
+john_list = [line.rstrip("\n").split() for line in john_infile]
+
+searches = {}
+for line in john_list:
+    for word in search:
+        searches[word] = str(line.count(word))
+
+for key in searches:
+    print(key + ": " + searches[key])
